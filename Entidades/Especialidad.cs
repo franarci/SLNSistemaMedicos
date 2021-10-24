90da9646207d59065ad7f2a008abaa410d120bbb
franarci <fran.arciniega96@gmail.com>
2021-10-23 21:17:50 -0300

    clase Especialidad

--- Entidades/Especialidad.cs
+++ Entidades/Especialidad.cs
@@ -6,7 +6,9 @@ using System.Threading.Tasks;
 
 namespace Entidades
 {
-    public class Class1
+    public class Especialidad
     {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
     }
 }
