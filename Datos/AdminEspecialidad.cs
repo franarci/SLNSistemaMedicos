5127e9205626361134c212069b445644ffe9d58d
franarci <fran.arciniega96@gmail.com>
2021-10-23 21:58:00 -0300

    preparo metodos AdminEspecialidad

--- Datos/AdminEspecialidad.cs
+++ Datos/AdminEspecialidad.cs
@@ -1,12 +1,31 @@
-﻿using System;
+﻿using Entidades;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Datos
 {
-    class AdminEspecialidad
+    public static class AdminEspecialidad
     {
+        public static DataTable Listar()
+        {
+            //TODO Listar()
+            return null;
+        }
+
+        public static DataTable TraerUno(int id)
+        {
+            //TODO TraerUno(id)
+            return null;
+        }
+
+        public static int Crear(Especialidad especialidad)
+        {
+            //TODO Crear(especialidad)
+            return 0;
+        }
     }
 }
