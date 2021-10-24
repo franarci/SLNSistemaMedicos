497c5773e975d57457984e52ec1ea543f7f3220a
franarci <fran.arciniega96@gmail.com>
2021-10-23 21:38:48 -0300

    preparo metodos de AdminMedico

--- Datos/AdminMedico.cs
+++ Datos/AdminMedico.cs
@@ -1,12 +1,37 @@
-﻿using System;
+﻿using Entidades;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Datos
 {
-    class AdminMedico
+    public static class AdminMedico
     {
+        public static DataTable Listar()
+        {
+            //TODO Listar()
+            return null;
+        }
+
+        public static DataTable Listar(int idEspecialidad)
+        {
+            //TODO Listar(idEspecialidad)
+            return null;
+        }
+
+        public static DataTable TraerUno(int id)
+        {
+            //TODO TraerUno(id)
+            return null;
+        }
+
+        public static int Crear(Medico medico)
+        {
+            //TODO Crear(medico)
+            return 0;
+        }
     }
 }
