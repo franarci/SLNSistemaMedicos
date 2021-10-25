3a043fbf9002897e3b7295bec8372fadbcc610a4
franarci <fran.arciniega96@gmail.com>
2021-10-25 18:18:08 -0300

    Creo metodo generico LlenarCombo(combo). Comportamiento de los combos

--- WindowsMedicos/frmMedicos.Designer.cs
+++ WindowsMedicos/frmMedicos.Designer.cs
@@ -31,6 +31,15 @@
             this.gridMedicos = new System.Windows.Forms.DataGridView();
             this.label1 = new System.Windows.Forms.Label();
             this.cbEspecialidad = new System.Windows.Forms.ComboBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtMatricula = new System.Windows.Forms.TextBox();
+            this.txtApellido = new System.Windows.Forms.TextBox();
+            this.cbEspecialidadCrear = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnGuardar = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.gridMedicos)).BeginInit();
             this.SuspendLayout();
             // 
@@ -45,7 +54,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(89, 121);
+            this.label1.Location = new System.Drawing.Point(269, 138);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(113, 13);
             this.label1.TabIndex = 1;
@@ -54,16 +63,101 @@
             // cbEspecialidad
             // 
             this.cbEspecialidad.FormattingEnabled = true;
-            this.cbEspecialidad.Location = new System.Drawing.Point(291, 118);
+            this.cbEspecialidad.Location = new System.Drawing.Point(411, 130);
             this.cbEspecialidad.Name = "cbEspecialidad";
             this.cbEspecialidad.Size = new System.Drawing.Size(121, 21);
             this.cbEspecialidad.TabIndex = 2;
+            this.cbEspecialidad.SelectedIndexChanged += new System.EventHandler(this.cbEspecialidad_SelectedIndexChanged);
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(114, 27);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(117, 20);
+            this.txtNombre.TabIndex = 3;
+            // 
+            // txtMatricula
+            // 
+            this.txtMatricula.Location = new System.Drawing.Point(261, 53);
+            this.txtMatricula.Name = "txtMatricula";
+            this.txtMatricula.Size = new System.Drawing.Size(121, 20);
+            this.txtMatricula.TabIndex = 4;
+            // 
+            // txtApellido
+            // 
+            this.txtApellido.Location = new System.Drawing.Point(114, 53);
+            this.txtApellido.Name = "txtApellido";
+            this.txtApellido.Size = new System.Drawing.Size(117, 20);
+            this.txtApellido.TabIndex = 5;
+            // 
+            // cbEspecialidadCrear
+            // 
+            this.cbEspecialidadCrear.FormattingEnabled = true;
+            this.cbEspecialidadCrear.Location = new System.Drawing.Point(261, 26);
+            this.cbEspecialidadCrear.Name = "cbEspecialidadCrear";
+            this.cbEspecialidadCrear.Size = new System.Drawing.Size(121, 21);
+            this.cbEspecialidadCrear.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(38, 33);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Nombre";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(38, 60);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Apellido";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(408, 30);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(67, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Especialidad";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(408, 60);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(67, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "NroMatricula";
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Location = new System.Drawing.Point(261, 80);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(121, 23);
+            this.btnGuardar.TabIndex = 11;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
             // 
             // frmMedicos
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cbEspecialidadCrear);
+            this.Controls.Add(this.txtApellido);
+            this.Controls.Add(this.txtMatricula);
+            this.Controls.Add(this.txtNombre);
             this.Controls.Add(this.cbEspecialidad);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.gridMedicos);
@@ -81,5 +175,14 @@
         private System.Windows.Forms.DataGridView gridMedicos;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.ComboBox cbEspecialidad;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtMatricula;
+        private System.Windows.Forms.TextBox txtApellido;
+        private System.Windows.Forms.ComboBox cbEspecialidadCrear;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnGuardar;
     }
 }
--- WindowsMedicos/frmMedicos.cs
+++ WindowsMedicos/frmMedicos.cs
@@ -22,27 +22,54 @@ namespace WindowsMedicos
         private void frmMedicos_Load(object sender, EventArgs e)
         {
             MostrarMedicos();
-            LlenarCombo();
+. compo            LlenarCombo(cbEspecialidadCrear);
+            
+            //uso el metodo llenar combo generico y le agrego una fila [Todas]
+            DataTable filtrarEspecialidad = LlenarCombo(cbEspecialidad);
+            DataRow dataRow = filtrarEspecialidad.NewRow();
+            dataRow["Nombre"] = "[Todas]";
+            filtrarEspecialidad.Rows.InsertAt(dataRow, 0);
         }
 
-        private void LlenarCombo()
+        private DataTable LlenarCombo(ComboBox combo)
         {
-
-
             DataTable Especialidad = AdminEspecialidad.Listar();
-
             
-            cbEspecialidad.DisplayMember = Especialidad.Columns["Nombre"].ToString();
-            cbEspecialidad.ValueMember = Especialidad.Columns["Id"].ToString();
-            cbEspecialidad.DataSource = Especialidad;
-            DataRow dataRow = Especialidad.NewRow();
-            dataRow["Nombre"] = "[Todas]";
-            Especialidad.Rows.InsertAt(dataRow, 0);
+            combo.DisplayMember = Especialidad.Columns["Nombre"].ToString();
+            combo.ValueMember = Especialidad.Columns["Id"].ToString();
+            combo.DataSource = Especialidad;
+            return Especialidad;
         }
 
         private void MostrarMedicos()
         {
             gridMedicos.DataSource= AdminMedico.Listar();
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            Medico medico = new Medico(txtNombre.Text, txtApellido.Text, Convert.ToInt32(txtMatricula.Text), Convert.ToInt32(cbEspecialidadCrear.SelectedValue));
+
+            int filasAfectadas = AdminMedico.Crear(medico);
+
+            if (filasAfectadas > 0)
+            {
+                MostrarMedicos();
+            }
+        }
+
+        private void cbEspecialidad_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string especialidad = cbEspecialidad.SelectedValue.ToString();
+
+            if (especialidad == "")
+            {
+                MostrarMedicos();
+            }
+            else
+            {
+                gridMedicos.DataSource = AdminMedico.Listar(Convert.ToInt32(cbEspecialidad.SelectedValue));
+            }
         }
     }
 }
