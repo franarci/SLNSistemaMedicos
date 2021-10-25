d54dbe8f414e8523e229f1da5b3a4c3d21ef587b
franarci <fran.arciniega96@gmail.com>
2021-10-25 15:34:57 -0300

    metodos crear, eliminar, modificar

--- Datos/AdminMedico.cs
+++ Datos/AdminMedico.cs
@@ -1,7 +1,9 @@
-﻿using Entidades;
+﻿using Datos.Servidor;
+using Entidades;
 using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,10 +12,16 @@ namespace Datos
 {
     public static class AdminMedico
     {
-        public static DataTable Listar()
+        public static List<Medico> Listar()
         {
-            //TODO Listar()
-            return null;
+            string consulta = "SELECT Nombre,Apellido,NroMatricula,IdEspecialidad FROM dbo.Medico";
+            SqlDataAdapter adapter = new SqlDataAdapter(consulta, AdminDB.ConectarBase());
+            List<Medico> medicos = new List<Medico>()
+            {
+                new Medico("Pedro", "Perez", 32, 2),
+                new Medico("Carla", "Arce", 15, 1)
+            };
+            return medicos;
         }
 
         public static DataTable Listar(int idEspecialidad)
@@ -30,8 +38,54 @@ namespace Datos
 
         public static int Crear(Medico medico)
         {
-            //TODO Crear(medico)
-            return 0;
+
+
+            string insertSQL = "INSERT dbo.Medico(Nombre,Apellido,NroMatricula, IdEspecialidad) VALUES(@Nombre, @Apellido, @NroMatricula, @IdEspecialidad)";
+
+            SqlCommand command = new SqlCommand(insertSQL, AdminDB.ConectarBase());
+
+            command.Parameters.Add("@Nombre", SqlDbType.VarChar, 50).Value = medico.Nombre;
+            command.Parameters.Add("@Apellido", SqlDbType.VarChar, 50).Value = medico.Apellido;
+            command.Parameters.Add("@NroMatricula", SqlDbType.Int).Value = medico.NroMatricula;
+            command.Parameters.Add("@IdEspecialidad", SqlDbType.Int).Value = medico.IdEspecialidad;
+
+            //ejecuta el insert
+            int filasAfectadas = command.ExecuteNonQuery();
+
+            AdminDB.ConectarBase().Close();
+            return filasAfectadas;
+        }
+
+        public static int Eliminar(int id)
+        {
+            string consulta = "DELETE FROM dbo.Medico WHERE Id=@Id";
+            SqlCommand command = new SqlCommand(consulta, AdminDB.ConectarBase());
+
+            command.Parameters.Add("@Id", SqlDbType.Int).Value = id;
+
+            //ejecuta el insert
+            int filasAfectadas = command.ExecuteNonQuery();
+
+            AdminDB.ConectarBase().Close();
+            return filasAfectadas;
+        }
+        public static int Modificar(Medico medico)
+        {
+            string consulta = "UPDATE dbo.Medico SET Nombre =@Nombre, Apellido=@Apellido, NroMatricula=@NroMatricula, IdEspecialidad= @IdEspecialidad WHERE Id=@Id";
+            SqlCommand command = new SqlCommand(consulta, AdminDB.ConectarBase());
+
+            command.Parameters.Add("@Nombre", SqlDbType.VarChar, 50).Value = medico.Nombre;
+            command.Parameters.Add("@Apellido", SqlDbType.VarChar, 50).Value = medico.Apellido;
+            command.Parameters.Add("@IdEspecialidad", SqlDbType.Int).Value = medico.IdEspecialidad;
+            command.Parameters.Add("@NroMatricula", SqlDbType.Int).Value = medico.NroMatricula;
+            command.Parameters.Add("@Id", SqlDbType.Int).Value = medico.Id;
+
+
+            //ejecuta el insert
+            int filasAfectadas = command.ExecuteNonQuery();
+
+            AdminDB.ConectarBase().Close();
+            return filasAfectadas;
         }
     }
 }
